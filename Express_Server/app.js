dd18ee9ef828f64f5f7187e5543a8eaca82946dd
Daniel <kontakt@danielrajic.de>
2018-01-16 21:33:07 +0100

    ejs added

--- Express_Server/app.js
+++ Express_Server/app.js
@@ -6,14 +6,26 @@ var express = require("express");
 // express ist eine Funktion und muss ausgeführt werden.
 var app = express();
 
+app.set("view engine", "ejs");
+//__dirname ist der Pfad zu app.js
+app.set("views",__dirname +"/views");
+
 //Middlewear agiert im Prinzip wie ein Filter in Java
-app.use("/middleware",function(req,res,next){
+app.use(function(req,res,next){
     console.log(req.url);
     next();
 });
 
 app.get("/",function(req, res){
-    res.send("Server is running...")
+    res.render("landing", {
+        title: "Node.JS",
+        items : [
+            "express",
+            "node",
+            "javascript"
+        ],
+        desc : "Ich bin eine Beschreibung"
+    }); //.ejs hinter Dateinamen kann weg gelassen werden.
 });
 
 app.get("/startseite", function (req,res){
